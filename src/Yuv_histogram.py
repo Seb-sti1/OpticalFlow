1ef5d57fcf36ee10514c8a19a245cc8ce8320f92
wnbilly <weynansbilly@gmail.com>
2023-03-23 19:33:12 +0100

    detection de changement de plan améliorée finie

--- src/Yuv_histogram.py
+++ src/Yuv_histogram.py
@@ -11,19 +11,49 @@ cap = cv2.VideoCapture('videos/Extrait1-Cosmos_Laundromat1(340p).m4v')
 if not cap.isOpened():
     print("Error loading video file")
 
+nb_hists = 5
+hist_idx = 0 # Keep track of idx of hist to add the last one in the array of hists with minimal computation
+threshold_gain = 4.5
+hists_array = np.zeros(shape=(nb_hists, 256, 256, 3))
+SAD = 0
+
+hist_idx = 0
 # Loop through the video frames
 while cap.isOpened():
     # Read the next frame from the video file
     ret, frame = cap.read()
 
-    ranges = [0, 255]
     frameYuv = cv2.cvtColor(frame, cv2.COLOR_BGR2YUV)
+
     # Don't forget the [] around the frame
     hist = cv2.calcHist([frameYuv], [1,2], None, [256, 256], [0,255, 0, 255])
+    # Log
     hist = np.log(hist)
-    hist = hist/ hist.max() * 255
-    hist = hist.astype(np.uint8) # Normalization to
+    # Normalization
+    hist = hist / hist.max() * 255
+    hist = hist.astype(np.uint8)
     hist = cv2.applyColorMap(hist, cv2.COLORMAP_JET)
+
+    # Sum of Absolute Differencies of the current histogram
+    SAD = np.sum(np.abs(hist - hists_array[hist_idx-1]))
+
+    # init threshold to 0
+    threshold = 0
+
+    # Sum of Absolute Differencies to compare the current and previous histograms
+    for i in range(1, nb_hists):
+        threshold += np.sum(np.abs(hists_array[i] - hists_array[i-1]))
+    # Adjustment of the threshold with threshold_gain
+    threshold = threshold_gain*threshold/nb_hists
+
+    if SAD > threshold :
+        print('\a') # Fait un bip sonore
+        print("Changement de plan       SAD : ", SAD, "      threshold", threshold,"        SAD/threshold", SAD/threshold)
+
+    # Add the current histogram to the list for the upcoming histogram of the next iteration
+    hists_array[hist_idx] = hist
+    # Update the hist_idx to access the latest histogram
+    hist_idx = hist_idx+1 if (hist_idx<nb_hists-1) else 0
 
     # Check if there are no more frames
     if not ret:
